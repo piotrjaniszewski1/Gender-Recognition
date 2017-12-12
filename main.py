95242c42cf9ee81d3aa45586e405c69dc6ee0301
Piotr <1piotr.janiszewski@wp.pl>
2017-12-12 22:21:38 +0100

    [B] Reading various files fixed

--- main.py
+++ main.py
@@ -6,18 +6,16 @@ from pylab import *
 import sys
 
 corePath = "/home/piotr/Desktop/PycharmProjects/Gender-Recognition/recordings/"
-
-freq = 44100
 chunkDuration = 0.02 #[s]
 
 generalFreqBand = 8000
 sectionFreqBand= 50
-numberOfSections = generalFreqBand // sectionFreqBand + 1
+numberOfSections = generalFreqBand // sectionFreqBand
 
 def prepareSamples(fileName):
 
     wavReader = open(fileName, 'r')
-    (_, sampwidth, _, nframes, _, _) = wavReader.getparams()
+    (_, sampwidth, framerate, nframes, _, _) = wavReader.getparams()
 
     # in case the whole program works too slow, read every second frame (according to the article, it is enough)
     frameData = wavReader.readframes(nframes)
@@ -25,11 +23,12 @@ def prepareSamples(fileName):
 
     format = {1: "%db", 2: "<%dh", 4: "<%dl"}[sampwidth] % samplesNumber
 
-    return unpack(format, frameData)
+    return unpack(format, frameData), framerate
 
 
-def createDataChunks(sampledData):
-    chunkLength = int(freq * chunkDuration)
+def createDataChunks(sampledData, freq):
+    chunkLength = int(floor(freq * chunkDuration))
+    dataLength = len(sampledData)
     dataLength = len(sampledData)
 
     return [sampledData[x:x + chunkLength] for x in range(0, dataLength, chunkLength)]
@@ -37,19 +36,18 @@ def createDataChunks(sampledData):
 
 def createEnergyDistribution(chunkedData):
 
-    chunkLenght = len(chunkedData[0])
-    numberOfSamplesInBand = chunkLenght / numberOfSections
-
     energyDistribution = list() # the list will contain the energy distribution of each of the 20 milisecond parts of our wave file
 
-    for x in chunkedData:
-        data = abs(np.fft.fft(x))
-
+    for chunk in chunkedData:
+        chunkLength = len(chunk)
+        numberOfSamplesInBand = chunkLength / numberOfSections
         localSummary = np.zeros(numberOfSections)
 
-        for i in range(chunkLenght):
+        data = abs(np.fft.fft(chunk))
+
+        for i in range(chunkLength):
             # adding particular amplitudes has the same result as adding particular energies (the mass is unknown, so we can't count the energy)
-            localSummary[int(floor(i / numberOfSamplesInBand))] += data[i]
+            localSummary[int(floor((i / numberOfSamplesInBand)))] += data[i]
 
         energyDistribution.append(localSummary)
 
@@ -60,8 +58,8 @@ def main():
 
     fileName = corePath + sys.argv[1]
 
-    sampledData = prepareSamples(fileName)
-    dataChunks = createDataChunks(list(sampledData))
+    sampledData, framerate = prepareSamples(fileName)
+    dataChunks = createDataChunks(list(sampledData), framerate)
     energyDistribution = createEnergyDistribution(dataChunks)
 
 if __name__ == '__main__':
